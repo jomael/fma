a1a1dda44129feb82c333d1a3503c8a6011bb92a
z_stoichev <z_stoichev@369c3813-ed14-0410-9e6a-8afa41335714>
2006-10-17 12:31:24 +0000

    More verbosed output and pause at the end.

--- UpdateSvnRevision.bat
+++ UpdateSvnRevision.bat
@@ -1,2 +1,10 @@
+@echo off
 REM CommandInterpreter: $(COMSPEC)
+echo ----------------OLD------------------
+type .\fma\trunk\uAbout.inc
+echo.
+echo ----------------NEW------------------
+echo Updating...
 "c:\Program Files\TortoiseSVN\bin\SubWCRev.exe" .\fma\trunk\ .\fma\trunk\uAbout.templ .\fma\trunk\uAbout.inc
+echo -------------------------------------
+pause
